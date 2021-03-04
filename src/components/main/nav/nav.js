32b8587a3890c17be48db0ef2940ffe6c9aa9292
Vincent Scavetta <vstro24@gmail.com>
2021-03-04 15:13:32 -0500

    using buttons instead of anchor tags for nav

--- src/components/main/nav/nav.js
+++ src/components/main/nav/nav.js
@@ -2,29 +2,27 @@ import React from 'react'
 import Scrollspy from 'react-scrollspy'
 import Scroll from './Scroll'
 
-//import "./nav.scss";
-
 const Nav = (props) => (
     <nav id="nav" className={props.sticky ? 'alt' : ''}>
         <Scrollspy items={ ['about', 'skills', 'projects', 'contact'] } currentClassName="is-active" offset={-300}>
             <li>
                 <Scroll type="id" element="about">
-                    <a href="#">About</a>
+                    <button>About</button>
                 </Scroll>
             </li>
             <li>
                 <Scroll type="id" element="skills">
-                    <a href="#">Skills</a>
+                    <button>Skills</button>
                 </Scroll>
             </li>
             <li>
                 <Scroll type="id" element="projects">
-                    <a href="#">Projects</a>
+                    <button>Projects</button>
                 </Scroll>
             </li>
             <li>
                 <Scroll type="id" element="contact">
-                    <a href="#">Contact Me</a>
+                    <button>Contact Me</button>
                 </Scroll>
             </li>
         </Scrollspy>
