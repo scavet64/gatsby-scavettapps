213e4080ae0beaa7cba897282e9e6db80450b3bf
Vincent Scavetta <vstro24@gmail.com>
2021-03-07 15:24:22 -0500

    Implementing privacy page with redirects to not break links

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+exports.createPages = ({ actions }) => {
+    const {createRedirect} = actions //actions is collection of many actions - https://www.gatsbyjs.org/docs/actions
+    createRedirect({ 
+        fromPath: '/privacy-policies/super-nova.html', 
+        toPath: '/privacy-policies/super-nova', 
+        redirectInBrowser: true,
+        isPermanent: true });
+  }
